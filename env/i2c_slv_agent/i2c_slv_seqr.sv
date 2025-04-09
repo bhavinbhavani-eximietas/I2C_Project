94ea928dc3b795cdcf8f63da3bc6050ad53e1d57
mitalprajapati-eximietas <mital.prajapati@eximietas.design>
2025-04-09 12:18:40 +0530

    changes in slv seqr

--- env/i2c_slv_agent/i2c_slv_seqr.sv
+++ env/i2c_slv_agent/i2c_slv_seqr.sv
@@ -7,15 +7,31 @@
 `define i2c_slv_seqr
 
 class i2c_slv_seqr extends uvm_sequencer #(i2c_slv_seq_item);
-	
+    
   // Factory registeration
   `uvm_component_utils(i2c_slv_seqr)
+
+  //export
+  uvm_analysis_export #(i2c_slv_seq_item) m_seqr_an_ex_port_h;
+  uvm_tlm_analysis_fifo #(i2c_slv_seq_item) m_fifo_h;
   
   extern function new(string name = "", uvm_component parent = null);
+  extern function void connect_phase(uvm_phase phase);
 
 endclass: i2c_slv_seqr
 `endif
 
 function i2c_slv_seqr::new(string name = "", uvm_component parent = null);
   super.new(name,parent);
+
+  m_seqr_an_ex_port_h = new("m_seqr_an_ex_port_h", this);
+  m_fifo_h = new("m_fifo_h", this);
+
 endfunction
+
+function void i2c_slv_seqr::connect_phase(uvm_phase phase);
+  super.connect_phase(phase);
+
+  m_seqr_an_ex_port_h.connect(m_fifo_h.analysis_export);
+
+endfunction: connect_phase
